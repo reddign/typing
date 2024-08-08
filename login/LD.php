75aaaebeaf8a704fbb5793f88fc213cbeda6bc63
Owen <FrybergerO1@etown.edu>
2024-08-08 09:54:04 -0400

    No Email/Verified

--- login/LD.php
+++ login/LD.php
@@ -1,11 +1,4 @@
 <?php
-session_start();
-require '../libs/sql.php';
-
-$username = $_POST["username"];
-$password = $_POST["password"];
-
-
 $query = $connection->query("SELECT * FROM users WHERE username='{$username}' and password=MD5('{$password}')");
 if (!$query) {
     die("Failed to connect to MySQL database");
@@ -17,8 +10,6 @@ if (sizeof($query) == 0) {
     exit();
 }
 
-// user exists, proceed to signin
-$_SESSION["LoggedIn"]="YES";
-$_SESSION["UserID"]=$_POST["username"];
-header("location:HomePage.html");
+$LD=$row["LD"];
+
 ?>
